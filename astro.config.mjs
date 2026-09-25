c38f93ccf8420e44d6ccb7f0107faeec57911f08
gungdikaebs <gungdika88@gmail.com>
2026-09-25 19:25:21 +0800

    feat: add GitHub Pages deployment workflow and configure site base URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://gungdikaebs.github.io",
+	base: "/muscle-up-membership",
 	output: "static",
 	vite: {
 		plugins: [tailwindcss()]
